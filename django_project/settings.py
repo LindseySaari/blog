b285a70a9b07a34908f9b0fd482986c96dea8016
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2022-10-20 10:08:24 -0400

    forms and user accounts

--- django_project/settings.py
+++ django_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "blog.apps.BlogConfig",  # new
+    "accounts.apps.AccountsConfig",
 ]
 
 
@@ -126,3 +127,6 @@ STATICFILES_DIRS = [BASE_DIR / "static"]  # new
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = "home"  # new
+LOGOUT_REDIRECT_URL = "home"  # new
